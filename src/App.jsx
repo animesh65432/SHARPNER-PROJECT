44105f7cbe6cc4acf6b88a1b471ddc530e67e997
animesh65432 <kiranduttt a234@gmail.com>
2023-12-24 17:09:55 +0530

    add-itmes

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Expense from "./Compoments/Expense";
+
+const App = () => {
+  return <Expense />;
+};
+export default App;
---
+++ src/Compoments/Expense.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Expense() {
+  return (
+    <div>
+      <p>EXPENSE ITEMS</p>
+      <p>FOOD RS 10</p>
+      <p>PETROL RS 100</p>
+      <p>MOVIES RS 200</p>
+    </div>
+  );
+}
+
+export default Expense;
